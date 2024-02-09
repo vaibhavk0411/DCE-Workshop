1b0273de1b61d3f18089c5147e6aafc8c0a69120
vaibhavk0411 <145129803+vaibhavk0411@users.noreply.github.com>
2024-02-09 14:18:47 +0530

    Update oddeven.sol

--- oddeven.sol
+++ oddeven.sol
@@ -1,1 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+contract ifelse{
+    function checkoddevent (int num) public view returns (int){
+        if(num%2==0){
+            return 1;
+        }
+        else{
+            return 0;
+        }
+    }
+}
