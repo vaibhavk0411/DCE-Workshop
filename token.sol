888c8ab10bebbdfdd8a3b3f8abb3135093944979
vaibhavk0411 <145129803+vaibhavk0411@users.noreply.github.com>
2024-02-09 14:43:26 +0530

    Create token.sol

---
+++ token.sol
@@ -0,0 +1,11 @@
+// contracts/GLDToken.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract RiseinCoin is ERC20 {
+    constructor() ERC20("DCE", "Vaibhav") {
+        _mint(msg.sender, 100000*(10**18));
+    }
+}
